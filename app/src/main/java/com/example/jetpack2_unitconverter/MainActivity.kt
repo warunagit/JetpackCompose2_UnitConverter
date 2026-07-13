adb573bf16e07f1d48881d01acd7a743c86691c0
warunagit <waruna.git@gmail.com>
2026-07-13 09:56:48 +0300

    initial

---
+++ app/src/main/java/com/example/jetpack2_unitconverter/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.jetpack2_unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack2_unitconverter.ui.theme.Jetpack2_unitconverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack2_unitconverterTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Box(){
+
+                    }
+
+                }
+            }
+        }
+    }
+}
